359d3d05a3df6cf8874b2691ababa2ce04627df8
Krishan Dwivedi <krishandwive9@gmail.com>
2023-09-03 00:15:49 +0530

    Added a way to add data into newItem

--- src/App.js
+++ src/App.js
@@ -25,14 +25,28 @@ function Logo() {
 }
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    setDescription("");
+    setQuantity(1);
+    console.log(newItem);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip ?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
